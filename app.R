3b0ad22996cbcdb6b0650792f5fc227b02419dad
zsmagyari <zsmagyari@gmail.com>
2023-05-07 20:14:18 +0300

    Add files via upload

--- app.R
+++ app.R
@@ -1,39 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinyjs)
 library(shinyFiles)
 library(leaflet)
 library(ncdf4)
 library(rgdal)
 library(raster)
 library(shinyWidgets)
+library(gsubfn)
 
+#considered file postfix for minimum, maximum, mean temperature and for precipitation 
+#data file format fileprefix_postfix.nc where fileprefix have to be the same for all files
 ab <- c("tn","tx","tg","rr")
+
+#number of considered indices
 indno <- 32
 
+#
+areaBoundary <- c(0,0,0,0)
+startYear <- 0
+endYear <- 0
+startMonth <- 1
+endMonth <- 12
 
-idNA <- function(file_name,var_name)
+fld <- ""
+
+pal <- colorBin(c("#f7fbff","#9ecae1","#4292c6","#2171b5","#08519c","#01ff01"), bins=c(0,0.99,0.992,0.994,0.996,0.998,1), na.color = "transparent",pretty=FALSE)
+
+getMapLimits <- function(folder,basename)
 {
-  nc <- nc_open(file_name) 
-  val <- ncvar_get(nc,var_name)
-  n <- nc$dim[[1]]$len
-  m <- nc$dim[[2]]$len  
-  for(i in 4:n)
-  {
-    for(j in 4:m)
-    {
-      l=which(is.na(val[i,j,]))
-      if (length(l)>0)
-        print(paste("lon:",i," lat:",j," day: ",l,sep=""))
-    }
-  }
+  nc <- nc_open(paste(folder,'/',basename,"_tx.nc",sep="")) 
+  lon <- ncvar_get(nc,nc$dim[[1]]$name)
+  lat <- ncvar_get(nc,nc$dim[[2]]$name)
+  nc_close(nc)
+  areaBoundary <<- c(min(lat),max(lat),min(lon),max(lon))
+}
+
+getDataInterval <- function(folder,basename)
+{
+  nc <- nc_open(paste(folder,'/',basename,"_tx.nc",sep="")) 
+  d1 <- strapplyc(nc$dim[[3]]$units, "[0-9-]{8,}", simplify = TRUE)
+  days <- nc$dim[[3]]$len
+  d2 <- as.Date(d1) + days
+  nc_close(nc)
+  startYear <<- year(d1)
+  endYear <<- year(d2)
+}
+
+checkAllFiles <- function(folder,mask)
+{
+  ok <- TRUE
+  for(i in 1:4)
+    ok <- ok & file.exists(paste(folder,"/",mask,"_",ab[i],".nc",sep=""))
+  return (ok)
 }
 
 
@@ -83,7 +101,7 @@ construct <- function(basename,abbrev,startingYear,endingYear,startingMonth,endi
   
   
   A <- array(rep(0,n*m*indno),c(n,m,indno))
-  print("Mean of mean T")  
+ 
   for(i in 1:n)
   {
     for(j in 1:m)
@@ -106,7 +124,8 @@ construct <- function(basename,abbrev,startingYear,endingYear,startingMonth,endi
       A[i,j,8] <- sum(fvdf[fvdf>30])/diff
     }
   }
-  print("Mean of min T")  
+
+   
   nc <- nc_open(paste(basename,"_",ab[1],".nc",sep="")) 
   val <- ncvar_get(nc,ab[1])
   nc_close(nc)
@@ -129,7 +148,8 @@ construct <- function(basename,abbrev,startingYear,endingYear,startingMonth,endi
       A[i,j,14] <- sum(fvdf[fvdf>10])/diff
     }
   }
-  print("Mean of max T")  
+
+  
   nc <- nc_open(paste(basename,"_",ab[2],".nc",sep="")) 
   val1 <- ncvar_get(nc,ab[2])
   nc_close(nc)
@@ -154,7 +174,7 @@ construct <- function(basename,abbrev,startingYear,endingYear,startingMonth,endi
     }
   }  
   
-  print("Mean of daily variation")  
+
   for(i in 1:n)
   {
     for(j in 1:m)
@@ -176,7 +196,6 @@ construct <- function(basename,abbrev,startingYear,endingYear,startingMonth,endi
   }    
   
   
-  print("Precipitation")  
   nc <- nc_open(paste(basename,"_",ab[4],".nc",sep="")) 
   val <- ncvar_get(nc,ab[4])
   nc_close(nc)
@@ -210,7 +229,7 @@ getRowColumn <- function(nc,lon,lat)
   llat <- ncvar_get(nc,nc$dim[[2]]$name)
   r <- which(lon<=llon)[1]
   c <- which(lat<=llat)[1]
-  return (c(r,c))
+  return (c(r,c-1))
 }
 
 
@@ -234,8 +253,6 @@ extract <- function(basename,abbrev,startingYear,endingYear,startingMonth,ending
   
   progress <<- progress + 1
   updateProgressBar(session=session, id="pb", value=progress)
-  print("Average T")  
-  
   
   vdf <- data.frame(tdf,value=val[i,j,])
   fvdf <- vdf$value[vdf$ye>=startingYear & vdf$ye<=endingYear & vdf$mo>=startingMonth & vdf$mo<=endingMonth]
@@ -245,7 +262,6 @@ extract <- function(basename,abbrev,startingYear,endingYear,startingMonth,ending
   progress <<- progress + 1
   updateProgressBar(session=session, id="pb", value=progress)
   
-  print("Min T")  
   nc <- nc_open(paste(basename,"_",ab[1],".nc",sep="")) 
   val <- ncvar_get(nc,ab[1])
   nc_close(nc)
@@ -257,7 +273,6 @@ extract <- function(basename,abbrev,startingYear,endingYear,startingMonth,ending
   progress <<- progress + 1
   updateProgressBar(session=session, id="pb", value=progress)
   
-  print("Max T")  
   nc <- nc_open(paste(basename,"_",ab[2],".nc",sep="")) 
   val1 <- ncvar_get(nc,ab[2])
   nc_close(nc)
@@ -269,7 +284,6 @@ extract <- function(basename,abbrev,startingYear,endingYear,startingMonth,ending
   progress <<- progress + 1
   updateProgressBar(session=session, id="pb", value=progress)
   
-  print("Daily variation")  
   vdf <- data.frame(tdf,value=val1[i,j,]-val[i,j,])
   fvdf <- vdf$value[vdf$ye>=startingYear & vdf$ye<=endingYear & vdf$mo>=startingMonth & vdf$mo<=endingMonth]
   
@@ -278,7 +292,6 @@ extract <- function(basename,abbrev,startingYear,endingYear,startingMonth,ending
   progress <<- progress + 1
   updateProgressBar(session=session, id="pb", value=progress)
   
-  print("Precipitation")  
   nc <- nc_open(paste(basename,"_",ab[4],".nc",sep="")) 
   val <- ncvar_get(nc,ab[4])
   nc_close(nc)
@@ -340,6 +353,7 @@ analyze <- function(loc,rect,basename,abbrev,target_startingYear,target_endingYe
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    useShinyjs(),
     tags$style(type = "text/css","h2 {text-align: center; font-size: 20px; font-weight: bold;} 
                                   h4 {font-size:14px; color: darkblue; font-weight: bold;} 
                                   label {font-size: 12px;}  
@@ -350,7 +364,7 @@ ui <- fluidPage(
                                   #mymap {cursor: pointer;}
                "),
     
-    titlePanel("ClimShift"),
+    titlePanel("C L I M S H I F T"),
     sidebarPanel(
       fluidRow(column(4,
         h4("Base period"),          
@@ -360,7 +374,7 @@ ui <- fluidPage(
         numericInput("bfmonth","Ending month",value=12,min=1,max=12),
         h4("Base location"),
         numericInput("lon","Longitude",value=23.12,min=22,max=25,step=0.01),  
-        numericInput("lat","Latitude",value=45.12,min=44,max=47,step=0.01)
+        numericInput("lat","Latitude",value=45.12,min=44,max=47,step=0.01),
       ),
       column(8,
              h4("Analysis period"),              
@@ -371,7 +385,6 @@ ui <- fluidPage(
              h4("Data files"),
              textInput("fname","Filename template"),
              actionButton("folder", "Choose folder"),
-             #shinyDirButton('folder', 'Állományok mappája', 'Válasszon mappát', FALSE),
              br(),
              hr(),
              br(),
@@ -397,15 +410,118 @@ ui <- fluidPage(
     
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
-
+  
+  shinyjs::disable("bsyear")
+  shinyjs::disable("bfyear")
+  shinyjs::disable("bsmonth")
+  shinyjs::disable("bfmonth")
+  shinyjs::disable("asyear")
+  shinyjs::disable("afyear")
+  shinyjs::disable("asmonth")
+  shinyjs::disable("afmonth")
+  shinyjs::disable("lon")
+  shinyjs::disable("lat")
+  shinyjs::disable("banalyze")
+  shinyjs::disable("bsave")
+  shinyjs::disable("fname")
+  
   
   observeEvent(input$folder, {
     fld <<- choose.dir()
+    updateTextInput(session=session,inputId="fname", value="")
+    if (!is.na(fld))
+    {
+      shinyjs::enable("fname")
+    }
+    else
+    {
+      shinyjs::disable("fname")
+    }
+  })
+  
+  observeEvent(input$fname, {
+    if (checkAllFiles(fld,input$fname))
+    {
+      getMapLimits(fld,input$fname)
+      updateNumericInput(session=session,inputId="lat",value=round(mean(areaBoundary[1:2]),digits=2))
+      updateNumericInput(session=session,inputId="lon",value=round(mean(areaBoundary[3:4]),digits=2))
+      
+      leafletProxy("mymap") %>% addRectangles(lng1=areaBoundary[3], lat1=areaBoundary[1], lng2=areaBoundary[4], lat2=areaBoundary[2], fillColor = "transparent",layerId="boundary")
+      
+      getDataInterval(fld,input$fname)
+      
+      updateNumericInput(session=session,inputId="bsyear",value=startYear)
+      updateNumericInput(session=session,inputId="asyear",value=startYear)
+      updateNumericInput(session=session,inputId="bfyear",value=endYear)
+      updateNumericInput(session=session,inputId="afyear",value=endYear)
+      
+      updateNumericInput(session=session,inputId="bsmonth",value=startMonth)
+      updateNumericInput(session=session,inputId="asmonth",value=startMonth)
+      updateNumericInput(session=session,inputId="bfmonth",value=endMonth)
+      updateNumericInput(session=session,inputId="afmonth",value=endMonth)
+      
+            
+      shinyjs::enable("bsyear")
+      shinyjs::enable("bfyear")
+      shinyjs::enable("bsmonth")
+      shinyjs::enable("bfmonth")
+      shinyjs::enable("asyear")
+      shinyjs::enable("afyear")
+      shinyjs::enable("asmonth")
+      shinyjs::enable("afmonth")
+      shinyjs::enable("lon")
+      shinyjs::enable("lat")
+      shinyjs::enable("banalyze") 
+    }
+    else
+    {
+      leafletProxy("mymap") %>% removeShape("boundary")
+      
+      shinyjs::disable("bsyear")
+      shinyjs::disable("bfyear")
+      shinyjs::disable("bsmonth")
+      shinyjs::disable("bfmonth")
+      shinyjs::disable("asyear")
+      shinyjs::disable("afyear")
+      shinyjs::disable("asmonth")
+      shinyjs::disable("afmonth")
+      shinyjs::disable("lon")
+      shinyjs::disable("lat")   
+      shinyjs::disable("banalyze") 
+    }
   })
   
   
+  listenCoord <-function()
+  {
+    list(input$lat,input$lon)
+  }
+  
+  observeEvent(listenCoord(),
+               {
+                 if (!is.na(input$lon))
+                 if (input$lat>=areaBoundary[1] & input$lat<=areaBoundary[2] & input$lon>=areaBoundary[3] & input$lon<=areaBoundary[4])
+                 {
+                   leafletProxy("mymap") %>% clearMarkers() 
+                   leafletProxy("mymap") %>% addCircleMarkers(lat=input$lat,lng=input$lon,fillColor="red",stroke =F,fillOpacity=1,radius=5)
+                   shinyjs::enable("banalyze") 
+                 }
+                 else
+                 {
+                   leafletProxy("mymap") %>% clearMarkers() 
+                   shinyjs::disable("banalyze") 
+                 } 
+               })
+  
   observeEvent(input$banalyze, {
+    
+    output$baseArea <- renderText("")
+    output$analysisArea <- renderText("")
+    output$commonArea <- renderText("")
+    
+    leafletProxy("mymap") %>% clearGroup("Analysis_period")
+    leafletProxy("mymap") %>% clearGroup("Base_period")
+    
     lon <<- input$lon
     lat <<- input$lat
     progress <<- 0
@@ -422,26 +538,23 @@ server <- function(input, output, session) {
     base <<-analyze(loc,rect,name,ab,input$bsyear,input$bfyear,input$bsmonth,input$bfmonth,input$bsyear,input$bfyear,input$bsmonth,input$bfmonth,lon,lat,name,session)
     rez <<-analyze(loc,rect,name,ab,input$asyear,input$afyear,input$asmonth,input$afmonth,input$bsyear,input$bfyear,input$bsmonth,input$bfmonth,lon,lat,name,session)
     
-    pal <- colorBin(c("#f7fbff","#9ecae1","#4292c6","#2171b5","#08519c","#01ff01"), bins=c(0,0.99,0.992,0.994,0.996,0.998,1), na.color = "transparent",pretty=FALSE)
-
     leafletProxy("mymap") %>% addRasterImage(flip(rez), group="Analysis_period", colors=pal, opacity = 0.5) 
     leafletProxy("mymap") %>% addRasterImage(flip(base), group="Base_period", colors=pal, opacity = 0.5)
-    leafletProxy("mymap") %>%  addLegend(pal=pal, values = c(0,0.99,0.992,0.994,0.996,0.998,1), title = "Similarity index")
-    
+
     
     cellno <- ncell(base)
     
     
-    bArea <- length(which(values(base)>0.998 & values(base)<=1))
+    bArea <<- length(which(values(base)>0.998 & values(base)<=1))
     
     updateProgressBar(session=session, id="pb", value=97)
     
-    aArea <- length(which(values(rez)>0.998 & values(rez)<=1))
+    aArea <<- length(which(values(rez)>0.998 & values(rez)<=1))
     
     updateProgressBar(session=session, id="pb", value=98)
     
     r <- (values(base)>0.998 & values(base)<=1) * (values(rez)>0.998 & values(rez)<=1)
-    cArea <- sum(r,na.rm=TRUE)
+    cArea <<- sum(r,na.rm=TRUE)
     
     updateProgressBar(session=session, id="pb", value=99)
     
@@ -450,32 +563,69 @@ server <- function(input, output, session) {
     output$commonArea <- renderText(paste("Common cell number with highest similarity: ",cArea," (",round(cArea/bArea*100,1),"% of base period cells)",sep=""))
     
     updateProgressBar(session=session, id="pb", value=100)
-    print("over")
+
+    shinyjs::enable("bsave") 
+    
   })
   
   observeEvent(input$bsave, {
     writeRaster(flip(rez),paste(name,"_lon",lon,"_lat",lat,"_B",input$bsyear,"_",input$bfyear,"_",input$bsmonth,"_",input$bfmonth,"_A",input$asyear,"_",input$afyear,"_",input$asmonth,"_",input$afmonth,sep=""),overwrite=TRUE)
     writeRaster(flip(base),paste(name,"_lon",lon,"_lat",lat,"_B",input$bsyear,"_",input$bfyear,"_",input$bsmonth,"_",input$bfmonth,sep=""),overwrite=TRUE)
-  })
+    
+    fileConn<-file(paste(name,"_lon",lon,"_lat",lat,"_B",input$bsyear,"_",input$bfyear,"_",input$bsmonth,"_",input$bfmonth,"_A",input$asyear,"_",input$afyear,"_",input$asmonth,"_",input$afmonth,".txt",sep=""))
+    writeLines(c(paste("Base period cell number:",bArea,sep=""),paste("Analysed period cell number:",aArea,sep=""),paste("Common cell number with highest similarity:",cArea,sep="")), fileConn)
+    close(fileConn)
+    
+    shinyjs::disable("bsave") 
+    
+    })
+  
+
   
   observe({
     click = input$mymap_click
     if(is.null(click))
       return()
     
-    updateNumericInput(session,"lon",value = click$lng)
-    updateNumericInput(session,"lat",value = click$lat)
+    if (click$lat>=areaBoundary[1] & click$lat<=areaBoundary[2] & click$lng>=areaBoundary[3] & click$lng<=areaBoundary[4])
+    {
+    
+    updateNumericInput(session,"lon",value = round(click$lng,digits=2))
+    updateNumericInput(session,"lat",value = round(click$lat,digits=2))
     leafletProxy("mymap") %>% clearMarkers()
     leafletProxy("mymap") %>% addCircleMarkers(lat=click$lat,lng=click$lng,fillColor="red",stroke =F,fillOpacity=1,radius=5)
-    
+    }
     })
   
+  listenPeriod <-function()
+  {
+    list(input$bsyear,input$bfyear,input$bsmonth,input$bfmonth,input$asyear,input$afyear,input$asmonth,input$afmonth)
+  }
+  
+  observeEvent(listenPeriod(),
+               {
+                 if (input$bsyear>=startYear & input$bsyear<=endYear & input$asyear>=startYear & input$asyear<=endYear &
+                     input$bfyear>=startYear & input$bfyear<=endYear & input$afyear>=startYear & input$afyear<=endYear &
+                     input$bsmonth>=startMonth & input$bsmonth<=endMonth & input$asmonth>=startMonth & input$asmonth<=endMonth &
+                     input$bfmonth>=startMonth & input$bfmonth<=endMonth & input$afmonth>=startMonth & input$afmonth<=endMonth
+                     )
+                 {
+                   shinyjs::enable("banalyze") 
+                 }
+                 else
+                 {
+                   shinyjs::disable("banalyze")  
+                 }
+               })
+  
+
   output$mymap <- renderLeaflet({
-    leaflet() %>% removeTiles(layerId = "rez")
-    leaflet() %>% addTiles(group="OSM") %>%  addLayersControl(baseGroups = c("OSM"),overlayGroups = c("Base_period", "Analysis_period"),options = layersControlOptions(collapsed = FALSE))  %>% setView(20.960933,46.585830, zoom=7)
+    #leaflet() %>% removeTiles(layerId = "rez")
+    leaflet() %>% addTiles(group="OSM") %>%  addLayersControl(baseGroups = c("OSM"),overlayGroups = c("Base_period", "Analysis_period"),options = layersControlOptions(collapsed = FALSE))  %>% setView(20.960933,46.585830, zoom=7) %>%  addLegend(pal=pal, values = c(0,0.99,0.992,0.994,0.996,0.998,1), title = "Similarity index")
+
   })
   
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server,options=list(launch.browser=TRUE))
